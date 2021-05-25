afbf25983c3e963bdec501a3f4bedbe73cfa921f
Arkon Digital <arkon.digital.hcf@gmail.com>
2021-05-26 04:26:26 +0800

    demo code for simple RichText usage, with an extra formatting button

--- src/editor.js
+++ src/editor.js
@@ -1,48 +1,126 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, RichText, RichTextToolbarButton } from '@wordpress/block-editor';
+import { toggleFormat, registerFormatType } from '@wordpress/rich-text';
 import './editor.scss'; 
+
+let formatName = 'myfirstblock/underline-format';
+
+const UnderlineButton = ( props ) => {
+  return (
+    <RichTextToolbarButton
+      icon="editor-underline"
+      title="Underline"
+      onClick={ () => {
+        props.onChange(
+          toggleFormat( props.value, {
+            type: formatName
+          } ) 
+        );
+      } }
+      isActive={ props.isActive }
+    />
+  );
+};
+ 
+registerFormatType( formatName, {
+  title: 'Underline',
+  tagName: 'u',
+  className: null,
+  edit: UnderlineButton,
+} );
 
 registerBlockType( 'myfirstblock/gtg-demo', {
   apiVersion: 2,
-  title: 'GTG Demo Block',
+  title: 'GTG Demo Block(RichText)',
   icon: 'heading', // https://developer.wordpress.org/resource/dashicons/#heading
   category: 'text',
   attributes: {
-    text: {
+    subheading: {
       type: 'string',
-      default: 'Hell World!'
     },
-    shit: {
+    heading: {
       type: 'string',
-      source: 'attribute',
-      selector: 'h1',
-      attribute: 'shit',
     },
-    dish: {
-      type: 'string'
+    content: {
+      type: 'string',
     }
   },
   edit: props => {
     const blockProps = useBlockProps( {
-      className: 'gtg-demo-h1',
-      'data-id': 'special-h1-id'
+      className: 'gtg-demo-card'
     } );
-    if (!props.attributes.shit) {
-      props.setAttributes({shit: 'color: yellow'})
-    }
-    if (!props.attributes.dish) {
-      props.setAttributes({dish: 'Needs washed'})
-    }
+
+    const {
+      attributes,
+      setAttributes
+    } = props;
+
+    const {
+      subheading,
+      heading,
+      content
+    } = attributes;
+
     return (
-      <h1 {...blockProps} shit={props.attributes.shit}>{props.attributes.text} {props.attributes.dish}</h1>
+      <div {...blockProps}>
+        <RichText
+          tagName='h3'
+          className='card-subheading'
+          value={subheading}
+          onChange={(newVal) => setAttributes({subheading: newVal})}
+          placeholder="Subheading Goes Here"
+        />
+        <RichText
+          tagName='h1'
+          className='card-heading'
+          value={heading}
+          onChange={(newVal) => setAttributes({heading: newVal})}
+          placeholder="Heading Goes Here"
+        />
+        <RichText
+          tagName='div'
+          className='card-content'
+          multiline='p'
+          value={content}
+          onChange={(newVal) => setAttributes({content: newVal})}
+          placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
     );
   },
   save: props => {
     const blockProps = useBlockProps.save( {
-      className: 'gtg-demo-h1',
+      className: 'gtg-demo-card',
     } );
+
+    const {
+      attributes
+    } = props;
+
+    const {
+      subheading,
+      heading,
+      content
+    } = attributes;
+
     return (
-      <h1 {...blockProps} shit={props.attributes.shit}>{props.attributes.text} {props.attributes.dish}</h1>
+      <div {...blockProps}>
+        <RichText.Content
+          tagName='h3'
+          className='card-subheading'
+          value={subheading}
+        />
+        <RichText.Content
+          tagName='h1'
+          className='card-heading'
+          value={heading}
+        />
+        <RichText.Content
+          tagName='div'
+          className='card-content'
+          value={content}
+        />
+      </div>
     );
   },
 } );
